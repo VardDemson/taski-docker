6416a477d1e242ce9699153f651936174b40d525
Aleksey Bespavlyuk <bespavlyuka@sfera.local>
2024-07-21 23:57:04 +0300

    bugfix v2

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -1,12 +1,15 @@
 import os
+
+from dotenv import load_dotenv
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+load_dotenv()
 
 SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^'
 
-DEBUG = True
+DEBUG = os.environ['DEBUG'].lower()
 
 ALLOWED_HOSTS = []
 
